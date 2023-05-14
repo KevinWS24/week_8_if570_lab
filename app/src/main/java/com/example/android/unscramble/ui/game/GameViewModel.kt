87607f35e0b49fde0d7ab50b9375a02beb1ed1a1
KevinWS24 <kevin.sumargo@student.umn.ac.id>
2023-05-14 15:57:51 +0700

    Populate ViewModel and Dialogs

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -20,17 +20,20 @@ import android.text.Spannable
 import android.text.SpannableString
 import android.text.style.TtsSpan
 import android.util.Log
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import com.example.android.unscramble.R
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 /**
  * ViewModel containing the app data and methods to process the data
  */
 class GameViewModel : ViewModel() {
-    private val _score = MutableLiveData(0)
-    val score: LiveData<Int>
+    private var _score = 0
+    val score: Int
         get() = _score
 
     private val _currentWordCount = MutableLiveData(0)
@@ -54,18 +57,15 @@ class GameViewModel : ViewModel() {
         }
     }
 
-    // List of words used in the game
+
     private var wordsList: MutableList<String> = mutableListOf()
     private lateinit var currentWord: String
 
     init {
         Log.d("GameFragment", "GameViewModel created!")
+        getNextWord()
     }
 
-
-    /*
-     * Updates currentWord and currentScrambledWord with the next word.
-     */
     private fun getNextWord() {
         currentWord = allWordsList.random()
         val tempWord = currentWord.toCharArray()
@@ -84,9 +84,21 @@ class GameViewModel : ViewModel() {
         }
     }
 
-    /*
-     * Re-initializes the game data to restart the game.
-     */
+    private fun showFinalScoreDialog() {
+        MaterialAlertDialogBuilder(requireContext())
+            .setTitle(getString(R.string.congratulations))
+            .setMessage(getString(R.string.you_scored, viewModel.score))
+            .setCancelable(false)
+            .setNegativeButton(getString(R.string.exit)) { _, _ ->
+                exitGame()
+            }
+            .setPositiveButton(getString(R.string.play_again)) { _, _ ->
+                restartGame()
+            }
+            .show()
+    }
+
+
     fun reinitializeData() {
         _score.value = 0
         _currentWordCount.value = 0
@@ -94,17 +106,10 @@ class GameViewModel : ViewModel() {
         getNextWord()
     }
 
-    /*
-    * Increases the game score if the player’s word is correct.
-    */
     private fun increaseScore() {
         _score.value = _score.value?.plus(SCORE_INCREASE)
     }
 
-    /*
-    * Returns true if the player word is correct.
-    * Increases the score accordingly.
-    */
     fun isUserWordCorrect(playerWord: String): Boolean {
         if (playerWord.equals(currentWord, true)) {
             increaseScore()
@@ -113,9 +118,6 @@ class GameViewModel : ViewModel() {
         return false
     }
 
-    /*
-    * Returns true if the current word count is less than MAX_NO_OF_WORDS
-    */
     fun nextWord(): Boolean {
         return if (_currentWordCount.value!! < MAX_NO_OF_WORDS) {
             getNextWord()
